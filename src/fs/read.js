d2c16b6362bbdc40d9682bd70291b8c1470a5f14
Volha Yakauleva <yvvolha@gmail.com>
2024-10-02 23:37:34 +0300

    feat: complete read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fsPromises from 'node:fs/promises';
+import path from 'path';
+import { ERROR_OPERATION_FAILED_TEXT } from '../constants.js';
+import { resolveDir, throwErrorWithText } from '../helpers.js';
+
 const read = async () => {
-    // Write your code here 
+    const dirPath = resolveDir('files', import.meta.url);
+    const filePath = path.join(dirPath, 'fileToRead.txt');
+
+    await fsPromises.access(filePath)
+        .catch((err) => throwErrorWithText(err, ERROR_OPERATION_FAILED_TEXT));
+
+    fsPromises.readFile(filePath, { encoding: 'utf8' })
+        .then((res) => {
+            console.log(res);
+        })
 };
 
 await read();
