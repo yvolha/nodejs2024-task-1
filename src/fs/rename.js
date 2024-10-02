0a5f12159b0d48b79d388b65146a63d5c68285a7
Volha Yakauleva <yvvolha@gmail.com>
2024-10-02 23:06:56 +0300

    feat: complete rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import fs from 'node:fs';
+import path from 'path';
+import { resolveDir, throwErrorWithText } from '../helpers.js';
+import { ERROR_OPERATION_FAILED_TEXT } from '../constants.js';
+
 const rename = async () => {
-    // Write your code here 
+    const wrongFilename = 'wrongFilename.txt';
+    const correctFilename = 'properFilename.md';
+
+    const dirPath = resolveDir('files', import.meta.url);
+    const fileWrongPath = path.join(dirPath, wrongFilename);
+    const fileCorrectPath = path.join(dirPath, correctFilename);
+
+    fs.access(
+        fileCorrectPath, 
+        (err) => {
+            if (!err) {
+                throwErrorWithText(err, ERROR_OPERATION_FAILED_TEXT)
+            }
+
+            fs.rename(
+                fileWrongPath, 
+                fileCorrectPath,
+                (err) => throwErrorWithText(err, ERROR_OPERATION_FAILED_TEXT),
+            )
+        }
+    )
 };
 
 await rename();
