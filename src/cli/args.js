ddad6e2d0c7cdd0e44cfb1927802608a18b6cc87
Volha Yakauleva <yvvolha@gmail.com>
2024-10-04 23:57:02 +0300

    feat: start doing args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argIndex = 2;
+
+    const cliArgs = process.argv.reduce((prev, next, index) => {
+
+        if (index < argIndex) {
+            return prev;
+        } else {
+            if (index % 2 === 0) {
+                return prev.push(next.substring(2) + ' is ');
+            } else {
+                return prev.push(next);
+            }
+        }
+    }, []);
+
+    console.log(cliArgs.slice(0, cliArgs.length - 2));
 };
 
 parseArgs();
