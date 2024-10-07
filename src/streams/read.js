8eb87192ae7c56d6831c6ff10969c22f2f1956ca
Volha Yakauleva <yvvolha@gmail.com>
2024-10-07 20:56:10 +0300

    feat: complete read using streams api task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { createReadStream } from 'node:fs';
+
+import { resolveDir } from '../helpers.js';
+
 const read = async () => {
-    // Write your code here 
+    const dirPath = resolveDir('files', import.meta.url);
+    const filePath = path.join(dirPath, 'fileToRead.txt');
+    const readStream = createReadStream(filePath);
+
+    let data = '';
+    readStream.on('data', (chunk) => {
+        data += chunk;
+    });
+    readStream.on('end', () => {
+        process.stdout.write(data + '\n\n');
+    })
 };
 
 await read();
