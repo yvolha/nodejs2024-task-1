3ae25de201bc73ed7e545063999db183d909786b
Volha Yakauleva <yvvolha@gmail.com>
2024-10-07 21:32:03 +0300

    feat: complete write using streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createInterface } from "node:readline"
+import { createWriteStream } from 'node:fs';
+import path from 'path';
+
+import { resolveDir } from "../helpers.js";
+
 const write = async () => {
-    // Write your code here 
+    const dirPath = resolveDir('files', import.meta.url);
+    const filePath = path.join(dirPath, 'fileToWrite.txt');
+    const writeStream = createWriteStream(filePath);
+   
+    for await (const line of createInterface({ input: process.stdin })) {
+        writeStream.write(line + '\n');
+    }
 };
 
 await write();
