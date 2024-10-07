5e5d9565890726a05f0d51b5fb0e07d39e9aba64
Volha Yakauleva <yvvolha@gmail.com>
2024-10-07 21:35:52 +0300

    feat: add a greeting message to writeable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -8,6 +8,8 @@ const write = async () => {
     const dirPath = resolveDir('files', import.meta.url);
     const filePath = path.join(dirPath, 'fileToWrite.txt');
     const writeStream = createWriteStream(filePath);
+
+    process.stdout.write("Please type something and press 'Enter' to save it in './files/fileToWrite.txt' \n");
    
     for await (const line of createInterface({ input: process.stdin })) {
         writeStream.write(line + '\n');
